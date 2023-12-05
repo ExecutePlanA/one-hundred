6b2e78bca9e0ff4212c32d24fd77e96e012e602a
ExecutePlanA <z1085582181@qq.com>
2023-12-06 00:06:45 +0800

    test

---
+++ Practice12_05/Practice12_05/practice.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+//打印1-100之间的奇数
+//#define MAX 100
+//
+//int main()
+//{
+//	int i = 1;
+//	for (i = 1; i <= MAX; i+=2)
+//	{
+//		if (i % 2 != 0)
+//		{
+//			printf("%d ", i);
+//		}
+//	}
+//	return 0;
+//}
+
+//打印9*9乘法表
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i <= 9; i++)
+//	{
+//		int j = 0;
+//		for (j = 1; j <= i; j++)
+//		{
+//			printf("%d*%d=%2d ", i, j, i * j);
+//		}
+//		printf("\n");
+//	}
+//
+//	return 0;
+//}
